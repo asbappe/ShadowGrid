dc58170dd003dcb2827c430f2b1cfc8b7754caa6
asbappe <asbappe@gmail.com>
2025-04-16 09:02:49 -0500

    Update ShadowGrid.py
    Fixing broken news links in ShadowWire

--- ShadowGrid.py
+++ ShadowGrid.py
@@ -172,5 +172,5 @@ with tab3:
         st.markdown("### Live Security Headlines")
         for item in news:
             st.markdown(f"**{item['Threat']}**")
-            st.markdown(f"[{item['Source']}]({item.get('link', '#')})")
+            st.markdown(f"[{item['Source']}]({item.get('link', '')})")
             st.markdown("---")
